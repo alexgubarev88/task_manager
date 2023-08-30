754a1ff4c9ecef05f5903e87c340a3057db3a92b
alexg <alexgubarev885@gmail.com>
2023-08-30 18:34:45 +0300

    Add models to task app task & tag

--- task/models.py
+++ task/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    deadline_date = models.DateTimeField(null=True, blank=True)
+    progres = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tags", blank=True)
+
+    def __str__(self) -> str:
+        return self.content
+
+    class Meta:
+        ordering = ["progres", "-created_date"]
